ec481421bc94d2281e1bc82675aeab33befb5da8
Abraham <abrahamemmanuelf@gmail.com>
2022-06-24 10:43:45 +0100

    These files contain the first push to my intro the javascript which prints out my name, height and country

---
+++ Java.js
@@ -0,0 +1,8 @@
+let Name = 'My name is Abraham Emmanuel';
+let Height = 'I am 2ft tall';
+let country = 'I am a citizen of Nigeria';
+
+
+console.log(Name);
+console.log(Height);
+console.log(country);
